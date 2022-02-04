447d1738718707d9546c9a4b3faa6e30d3f1e344
Louis Pearson <louispearson@librem.one>
2022-02-03 21:34:01 -0700

    Log midi messages

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Hex Midi Controller
 
 Midi controller built on web technologies with a hexagonal grid layout.
+
+Running this on mobile through termux using `python -m http.server`.
--- script.js
+++ script.js
@@ -6,25 +6,40 @@ class Logger {
   log(text) {
     this.#views.forEach((view) => view.append(text + "\n"));
   }
+  logPort(port) {
+    this.log(
+      `[${port.type.toUpperCase()}] id: ${port.id} manufacturer: ${
+        port.manufacturer
+      } name: ${port.name} version: ${port.version}`
+    );
+  }
 }
 
 const logger = new Logger();
+
+const onMIDIMessage = (message) => {
+  console.dir(message);
+  logger.log(message.data.join(" "));
+};
+
+const onStateChange = (change) => {
+  console.dir(change);
+  const port = change.port;
+  logger.logPort(port);
+};
 
 const load = async () => {
   try {
     const midi = await navigator.requestMIDIAccess();
     logger.log("Midi Ready!");
     console.dir(midi);
-    midi.inputs.forEach((input) =>
-      logger.log(
-        `[INPUT] type: ${input.type} id: ${input.id} manufacturer: ${input.manufacturer} name: ${input.name} version: ${input.version}`
-      )
-    );
-    midi.outputs.forEach((output) =>
-      logger.log(
-        `[OUTPUT] type: ${output.type} id: ${output.id} manufacturer: ${output.manufacturer} name: ${output.name} version: ${output.version}`
-      )
-    );
+    midi.onstatechange = onStateChange;
+    midi.inputs.forEach((input) => {
+      logger.logPort(input);
+      console.dir(input);
+      input.onmidimessage = onMIDIMessage;
+    });
+    midi.outputs.forEach((output) => logger.logPort(output));
   } catch (err) {
     logger.log(err);
   }
